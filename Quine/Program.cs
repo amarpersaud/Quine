ae27776748a08095a5a34754c02ec0bfd3e66d3c
amarpersaud <tehspartaa@gmail.com>
2018-01-07 19:26:39 -0500

    Actual initial Commit

---
+++ Quine/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+public class Program
+{
+    public static string s =
+    @"		using System;
+		public class Program
+		{{
+			public static string s = 
+			@""{0}"";
+			public static void Main(){{
+				string b = (""    "" + s.Replace(""\n"", ""\n"" + ""    "")).Replace(""\"""", ""\""\"""");
+				Console.WriteLine(String.Format(s, b));
+			}}
+		}}";
+    public static void Main()
+    {
+        string b = ("    " + s.Replace("\n", "\n" + "    ")).Replace("\"", "\"\"");
+        Console.WriteLine(String.Format(s.Replace("        ", "    "), b));
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Quine
+
+This is a quine written in C#. A quine is a program whose output is its source code. This quine keeps its formatting.
+
+This is my first try at a .NET Core application. The original code was written in http://dotnetfiddle.net, and moved over to this application.
